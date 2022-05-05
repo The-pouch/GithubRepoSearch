c4606e43ce460e1a5d0caf17207709f35cead6de
Aaron Edaku <eaaron25@gmail.com>
2022-05-05 11:10:29 +0300

    Done did the TOdos

--- app/src/main/java/com/inonidakii/githubreposearch/MainActivity.java
+++ app/src/main/java/com/inonidakii/githubreposearch/MainActivity.java
@@ -3,22 +3,22 @@ package com.inonidakii.githubreposearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (26) Create an EditText variable called mSearchBoxEditText
-
-    // TODO (27) Create a TextView variable called mUrlDisplayTextView
-    // TODO (28) Create a TextView variable called mSearchResultsTextView
+    EditText eSearchBoxEditText;
+    TextView eUrlDisplayTextView;
+    TextView eSearchResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
-
-        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
-        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        eSearchBoxEditText = findViewById(R.id.et_search_box);
+        eUrlDisplayTextView = findViewById(R.id.tv_url_display);
+        eSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
     }
 }
