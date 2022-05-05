8a5927f2fb5d89da6967026fa4ce011b30c905c5
Aaron Edaku <eaaron25@gmail.com>
2022-05-05 15:13:51 +0300

    Initial commit

--- app/src/main/java/com/inonidakii/githubreposearch/MainActivity.java
+++ app/src/main/java/com/inonidakii/githubreposearch/MainActivity.java
@@ -1,10 +1,15 @@
 package com.inonidakii.githubreposearch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,23 +27,27 @@ public class MainActivity extends AppCompatActivity {
         eSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
     }
 
-    // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
-    // TODO (2) Create a menu xml called 'main.xml' in the res->menu folder
-    // TODO (3) Add one menu item to your menu
-    // TODO (4) Give the menu item an id of @+id/action_search
-    // TODO (5) Set the orderInCategory to 1
-    // TODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
-    // TODO (7) Set the title to the search string ("Search") from strings.xml
-    // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
+    // TODO (2) Create a method called makeGithubSearchQuery
+    // TODO (3) Within this method, build the URL with the text from the EditText and set the built URL to the TextView
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
 
-    // TODO (8) Override onCreateOptionsMenu
-    // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
-    // TODO (10) Return true to display your menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
 
-    // TODO (11) Override onOptionsItemSelected
-    // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-    // TODO (14) Don't forgot to call .show() on your Toast
-    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+            // TODO (4) Remove the Toast message when the search menu item is clicked
+            Toast.makeText(this, getString(R.string.search), Toast.LENGTH_SHORT).show();
+            // TODO (5) Call makeGithubSearchQuery when the search menu item is clicked
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
