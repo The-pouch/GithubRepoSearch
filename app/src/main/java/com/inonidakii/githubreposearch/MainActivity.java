527b97b2b2545df9dbd4f83ae53c467b5544f40e
Aaron Edaku <eaaron25@gmail.com>
2022-05-10 17:36:13 +0300

    Done layout todos

--- app/src/main/java/com/inonidakii/githubreposearch/MainActivity.java
+++ app/src/main/java/com/inonidakii/githubreposearch/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     TextView eUrlDisplayTextView;
     TextView eSearchResultsTextView;
 
+    // TODO (12) Create a variable to store a reference to the error message TextView
+
+    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +34,18 @@ public class MainActivity extends AppCompatActivity {
         eSearchBoxEditText = findViewById(R.id.et_search_box);
         eUrlDisplayTextView = findViewById(R.id.tv_url_display);
         eSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+
+        // TODO (13) Get a reference to the error TextView using findViewById
+
+        // TODO (25) Get a reference to the ProgressBar using findViewById
     }
 
+    /**
+     * This method retrieves the search text from the EditText, constructs the
+     * URL (using {@link NetworkUtils}) for the github repository you'd like to find, displays
+     * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
+     * our {@link GithubQueryTask}
+     */
     void makeGithubSearchQuery() {
         String githubSearchQuery = eSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubSearchQuery);
@@ -41,7 +54,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // TODO (14) Create a method called showJsonDataView to show the data and hide the error
+
+    // TODO (15) Create a method called showErrorMessage to show the error and hide the data
+
     class GithubQueryTask extends AsyncTask<URL, Void, String>{
+        // TODO (26) Override onPreExecute to set the loading indicator to visible
         String response = null;
         @Override
         protected String doInBackground(URL... urls) {
@@ -56,8 +74,11 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
+            // TODO (27) As soon as the loading is complete, hide the loading indicator
+            // TODO (17) Call showJsonDataView if we have valid, non-null results
             super.onPostExecute(s);
             eSearchResultsTextView.setText(s);
+            // TODO (16) Call showErrorMessage if the result is null in onPostExecute
         }
     }
 
